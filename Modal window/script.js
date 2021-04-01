8520b9f7464cabb7ee7d690049966823e28b676f
Annamalai Palani <49332020+annshiv@users.noreply.github.com>
2021-04-01 11:14:58 +0530

    Initial without functions

--- Modal window/script.js
+++ Modal window/script.js
@@ -15,14 +15,6 @@ const closeModal = function() {
     overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnOpenModal.length; i++)
-    btnOpenModal[i].addEventListener('click',openModal);
-
-btnCloseModal.addEventListener('click',closeModal);
-overlay.addEventListener('click',closeModal);
-
-document.addEventListener('keydown',function(e) {
-    if(e.key === 'Escape' && !modal.classList.contains('hidden')) {
-        closeModal();
-    }
-});
+for (let i = 0; i < btnOpenModal.length; i++) {
+    console.log(i) 
+}
