f1a59911b76e57411af7c4b7e286e74a76a680dd
Annamalai Palani <49332020+annshiv@users.noreply.github.com>
2021-04-02 10:19:24 +0530

    Player 1 part functions

--- Pig Game/script.js
+++ Pig Game/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnRoll = document.querySelector('.btn--roll');
+const btnNew = document.querySelector('.btn--new');
+const btnHold = document.querySelector('.btn--hold');
+const current0El = document.getElementById('current--0');
+
+let currentScore = 0
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+btnRoll.addEventListener('click',function(){
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    if (dice !== 1) {
+        currentScore += dice;
+        current0El.textContent = currentScore
+    } else {
+
+    }
+})
