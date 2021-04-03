f227e0441e0824640d1c31a9e84428820286788a
Annamalai Palani <49332020+annshiv@users.noreply.github.com>
2021-04-03 09:11:59 +0530

    Switching player

--- Pig Game/script.js
+++ Pig Game/script.js
@@ -7,8 +7,15 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnNew = document.querySelector('.btn--new');
 const btnHold = document.querySelector('.btn--hold');
 const current0El = document.getElementById('current--0');
+const player0El = document.querySelector('.player--0')
+const player1El = document.querySelector('.player--1')
 
-let currentScore = 0
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+
+
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
@@ -21,8 +28,12 @@ btnRoll.addEventListener('click',function(){
 
     if (dice !== 1) {
         currentScore += dice;
-        current0El.textContent = currentScore
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
     } else {
-
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
     }
 })
