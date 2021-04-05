c135b095e87a362361309389b8eb298a89faa69b
Annamalai Palani <49332020+annshiv@users.noreply.github.com>
2021-04-05 11:55:47 +0530

    Resetting the game

--- Pig Game/script.js
+++ Pig Game/script.js
@@ -62,7 +62,7 @@ btnHold.addEventListener('click',function() {
         scores[activePlayer] += currentScore;
         document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
 
-        if (scores[activePlayer] >= 50) {
+        if (scores[activePlayer] >= 100) {
             playing = false;
             diceEl.classList.add('hidden');
             document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
