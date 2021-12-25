bedd47b12899b308b270537865c0c795cac7d9b2
Annamalai Palani <49332020+annshiv@users.noreply.github.com>
2021-12-25 19:53:37 +0530

    removed unused code

--- D3-visualization/fourth/src/chart.js
+++ D3-visualization/fourth/src/chart.js
@@ -16,75 +16,21 @@ function BarChart({ width, height, data, data1 }) {
   const func = () => {
     top ? draw() : draw1();
   };
-  // const height = top ? height - yScale(d) - 5
 
   const change = () => {
     setShowResults(!showResults);
   };
 
-  function position() {
-    setTop(!top);
-  }
-
   useEffect(() => {
-    // var selection = svg.selectAll("rect").data(data);
 
     d3.select(ref.current)
       .attr("width", width)
       .attr("height", height)
       .style("border", "1px solid black")
       .attr("fill", `${showResults ? "green" : "red"}`);
-    //   .attr("y", (d) => height - yScale(d) - 5);
-
-    // selection
-    //   .enter()
-    //   .append("rect")
-    //   .attr("x", (d, i) => i * 45)
-    //   .attr("y", (d) => height)
-    //   .attr("width", 40)
-    //   .attr("height", 0)
-    //   .attr("fill", "orange")
-    //   .attr("height", (d) => yScale(d))
-    //   .attr("y", (d) => height - yScale(d));
-
-    // selection
-    //   .data(data1)
-    //   .enter()
-    //   .append("rect")
-    //   .attr("class", "bar1")
-    //   .attr("x", (d, i) => i * 45)
-    //   .attr("y", (d) => height)
-    //   .attr("width", 40)
-    //   .attr("height", 0)
-    //   .attr("height", (d) => yScale(d))
-    //   .attr("y", (d) => height - yScale(d));
-
-    // svg
-    //   .selectAll("text")
-    //   .data(data)
-    //   .enter()
-    //   .append("text")
-    //   .text((d) => d)
-    //   .attr("x", (d, i) => i * 45)
-    //   .attr("y", (d) => height - yScale(d) - 5);
 
     func();
-    //   .style("font-size", 64)
-    //   .attr("name", "asdf");
   }, [showResults, top]);
-
-  //   const a = d3
-  //     .selectAll("text")
-  //     .data(data1)
-  //     .enter()
-  //     .append("text")
-  //     .text((d) => d)
-  //     .attr("x", (d, i) => i * 45)
-  //     .attr("y", (d) => height - yScale(d) - 5);
-
-  //   useEffect(() => {
-  //     draw();
-  //   }, [data, data1, text_color]);
 
   const draw = () => {
     const svg = d3.select(ref.current);
@@ -157,21 +103,6 @@ function BarChart({ width, height, data, data1 }) {
       .attr("x", (d, i) => i * 45)
       .attr("y", (d) => height - 10);
   };
-
-  //   const draw = () => {
-  // selection
-  //   .data(data1)
-  //   .enter()
-  //   .append("text")
-  //   .text((d) => d)
-  //   .attr("x", (d, i) => i * 45)
-  //   .attr("y", (d) => height - 5)
-  //   .attr("fill", "red");
-  //   };
-
-  //   console.log(showResults);
-  console.log(text_color);
-  //   console.log(top);
 
   return (
     <div className="chart">
